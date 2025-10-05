672d7c954748237c49dc10e2ec78e87ccedf3eac
Tick Zhong <1601858426@qq.com>
2025-10-05 10:42:55 +0800

    Kt练习

--- Family/src/Field.kt
+++ Family/src/Field.kt
@@ -2,7 +2,6 @@ open class Field (){
     var area: Double = 0.0
 
     open fun calculateArea() : Double {
-//        area = length * width
         return area
     }
 }
---
+++ Src/2N.kt
@@ -0,0 +1,20 @@
+import java.util.Scanner
+import kotlin.math.*
+
+fun main(args: Array<String>) {
+    val input = Scanner(System.`in`)
+    while (input.hasNextInt()) {
+        val x = abs(input.nextInt())
+        if (x == 0) {
+            println("No!")
+            continue
+        }
+        var temp = 1
+        var cnt = 0
+        while (temp < x) {
+            temp *= 2
+            cnt++
+        }
+        if (temp == x) println(cnt) else println("No!")
+    }
+}
